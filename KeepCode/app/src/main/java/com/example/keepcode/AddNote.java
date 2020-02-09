c28f11f302686217aab368525ba54e1f7963e830
jynlin3 <flin3@uw.edu>
2020-02-09 11:53:00 -0800

    Show Text view and Code view at "Edit" page.

--- KeepCode/app/src/main/java/com/example/keepcode/AddNote.java
+++ KeepCode/app/src/main/java/com/example/keepcode/AddNote.java
@@ -6,22 +6,31 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+
+import net.cryptobrewery.syntaxview.SyntaxView;
 
 public class AddNote extends AppCompatActivity {
     Toolbar toolbar;
     EditText noteTitle;
-    EditText noteDetails;
     int noteID;
+    ArrayList<SyntaxView> codeViews;
+    ArrayList<EditText> textViews;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,13 @@ public class AddNote extends AppCompatActivity {
         getSupportActionBar().setTitle("New Note");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        codeViews = new ArrayList<>();
+        textViews = new ArrayList<>();
         noteTitle = findViewById(R.id.noteTitle);
-        noteDetails = findViewById(R.id.noteDetails);
+        EditText noteDetails = findViewById(R.id.noteDetails);
+        textViews.add(noteDetails);
         noteID = -1;
+        layout = findViewById(R.id.content);
 
         Note note = (Note) getIntent().getSerializableExtra("note");
         if (note != null) {
@@ -59,7 +72,8 @@ public class AddNote extends AppCompatActivity {
             //TODO: Update note.
             Date currentTime = Calendar.getInstance().getTime();
             DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-            Note note = new Note(noteTitle.getText().toString(), noteDetails.getText().toString(),
+            //TODO: Save content of textviews and codeviews
+            Note note = new Note(noteTitle.getText().toString(), "",//noteDetails.getText().toString(),
                     dateFormat.format(currentTime), dateFormat.format(currentTime));
             NoteDatabase.getInstance(this).noteDao().insert(note);
             goToMain();
@@ -72,6 +86,34 @@ public class AddNote extends AppCompatActivity {
                         .show();
             }
             goToMain();
+        }
+        if(item.getItemId() == R.id.addCode)
+        {
+            //create SyntaxView
+            SyntaxView codeView = new SyntaxView(this);
+            codeView.setLayoutParams(new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            layout.addView(codeView);
+            codeViews.add(codeView);
+
+            //create EditText
+            EditText textView = new EditText(this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            //set margins of EditText with dp
+            int dpValue = 16;
+            float d = this.getResources().getDisplayMetrics().density;
+            int margin = (int) (dpValue * d);
+            lp.setMargins(margin,margin, margin, margin);
+            textView.setLayoutParams(lp);
+            textView.setBackground(null);
+            textView.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE);
+            textView.setMaxLines(4);
+            textView.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+
+            layout.addView(textView);
+            textViews.add(textView);
+
         }
         return super.onOptionsItemSelected(item);
     }
